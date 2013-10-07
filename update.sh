b3148867857c90cf50589a0475891983f7a8541b
Joel Van Eenwyk <joel.van.eenwyk@global.havok.com>
2013-10-07 15:01:50 -0700

    Added project files and other needed files for iOS build.

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mkdir -p ./Data/Vision
+cp -R -f "/Volumes/Data/$VISION_SDK/Data/Vision/Base" ./Data/Vision
+
+mkdir -p ./Data/Common/iOS
+cp -R -f "/Volumes/Data/$VISION_SDK/Data/Common/iOS" ./Data/Common
